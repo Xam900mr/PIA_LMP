f9dd3e4ea2afc4d5ee5ed10b8e98f6b7b485f6c9
Max Rada <xam900mr@gmail.com>
2024-11-03 00:11:10 -0600

    Sesion, agregar, y botones

--- PIA_LMP/Aplicacion_Crud/main.py
+++ PIA_LMP/Aplicacion_Crud/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
 import os
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, DateField, IntegerField
 
 db = mysql.connector.connect(
     user="root",
@@ -22,6 +22,17 @@ class LoginForm(Form):
     Contrasena = PasswordField('Contraseña', [validators.DataRequired()])
     remember = BooleanField('Recordarme')
 
+class BookForm(Form):     
+    Nombre = StringField('Nombre', [validators.DataRequired()])     
+    Autor = StringField('Autor', [validators.DataRequired()])     
+    isbn = StringField('ISBN', [validators.DataRequired()])     
+    edicion = IntegerField('Edición', [validators.DataRequired()])     
+    fecha_pub = DateField('Fecha de Publicación', [validators.DataRequired()], format='%Y-%m-%d')     
+    categoria = StringField('Categoría', [validators.DataRequired()]) 
+    cantidad = IntegerField('Cantidad', [validators.DataRequired()]) 
+    disponibilidad = IntegerField('Disponibilidad', [validators.DataRequired()]) 
+    editorial = StringField('Editorial', [validators.DataRequired()]) 
+
 
 
 app = Flask (__name__, template_folder='./Templates')
@@ -34,34 +45,30 @@ def index():
     cursor.execute("select * from Libros")
     Libros = cursor.fetchall()
 
-    return render_template('index.html', Libros=Libros)
+    user_id = session.get('user_id')
+    user_type = session.get('user_type')
+
+    return render_template('index.html', Libros=Libros, session=session, user_id=user_id, user_type=user_type)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
-        try:
-            cursor=db.cursor()
-            query = "SELECT ID, Contrasena FROM usuarios WHERE Correo = %s"
-            cursor.execute(query, (form.Correo.data,))
-            user = cursor.fetchone()
-            
-            # Asegúrate de consumir todos los resultados
-            cursor.fetchall()  # Limpia cualquier resultado pendiente
-            
-            if user and check_password_hash(user[1], form.Contrasena.data):
-                session['user_id'] = user[0]
-                flash('Inicio de sesión exitoso')
-                return redirect(url_for('index'))
-            else:
-                flash('Correo o contraseña incorrectos')
-                
-        except mysql.connector.Error as err:
-            flash('Error en la base de datos')
-            print(f"Error: {err}")
-            
-        finally:
-            cursor.close()  
+        cursor = db.cursor()
+        query = "SELECT Nombre, Contrasena, tipo FROM usuarios WHERE Correo = %s"
+        cursor.execute(query, (form.Correo.data,))
+        user = cursor.fetchone()
+        cursor.close()
+        
+        if user and check_password_hash(user[1], form.Contrasena.data):
+            session['user_id'] = user[0]
+            session['user_type'] = user[2]  
+            flash('Inicio de sesión exitoso', 'success')  
+            return redirect(url_for('index'))
+        else:
+            flash('Correo o contraseña incorrectos', 'danger') 
+            return redirect(url_for('login'))
+    
     return render_template('login.html', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -70,8 +77,8 @@ def register():
     if request.method == 'POST' and form.validate():         
         hashed_password = generate_password_hash(form.Contrasena.data)        
         query = """         
-        INSERT INTO usuarios (Nombre, Correo, Contrasena)         
-        VALUES (%s, %s, %s)         
+        INSERT INTO usuarios (Nombre, Correo, Contrasena, tipo)         
+        VALUES (%s, %s, %s, "u")         
         """        
         values = (form.Usuario.data, form.Correo.data, hashed_password)       
         cursor=db.cursor()         
@@ -80,6 +87,28 @@ def register():
         flash('Gracias por registrarte') 
         return redirect(url_for('login')) 
     return render_template('register.html', form=form)
+
+@app.route('/agregar', methods=['GET', 'POST'])
+def add_book():     
+    form = BookForm(request.form)    
+    if request.method == 'POST' and form.validate():    
+        query = """         
+        INSERT INTO libros (Nombre, Autor, ISBN, Edicion, Fecha_Pub, Categoria, Cantidad, Disponibilidad, editorial)         
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)         
+        """             
+        values = (form.Nombre.data, form.Autor.data, form.isbn.data, form.edicion.data, form.fecha_pub.data, form.categoria.data, form.cantidad.data, form.disponibilidad.data, form.editorial.data)
+        cursor=db.cursor()
+        cursor.execute(query, values)
+        db.commit()
+        flash('Libro agregado exitosamiente', 'success') 
+        return redirect(url_for('index')) 
+    return render_template('Agregar.html', form=form)
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)  
+    flash("Has cerrado sesión exitosamente")
+    return redirect(url_for('index'))
 
 if __name__=='__main__':
     app.run(debug=True)
